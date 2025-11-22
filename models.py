b76ff65cb018c9cb4034685f6d8c04a21a3a6b07
jamestamanikaiyaroi-ai <jamestamanikaiyaroi@gmail.com>
2025-11-22 18:10:29 +1200

    Create models.py

---
+++ models.py
@@ -0,0 +1,15 @@
+from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy.sql import func
+from database import Base
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    full_name = Column(String, nullable=False)
+    mobile_number = Column(String, unique=True, index=True, nullable=False)
+    email = Column(String, unique=True, index=True, nullable=True)
+    password_hash = Column(String, nullable=False)
+    role = Column(String, nullable=False, default="captain")  # captain|owner|authority
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
